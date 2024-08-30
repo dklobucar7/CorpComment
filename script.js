9f570835c6f2e150342a7b410cb001ac118b6d86
Danijel Klobucar <dklobucar@cadcam-group.eu>
2024-08-30 17:04:45 +0200

    added Form Component

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 // -- GLOBAL --
+const MAX_CHARS = 150;
+
 const counterEl = document.querySelector(".counter");
 const textareaEl = document.querySelector(".form__textarea");
 const formEl = document.querySelector(".form");
+const feedbackListEl = document.querySelector(".feedbacks");
+const submitBtnEl = document.querySelector(".submit-btn");
 
 // -- COUNTER COMPONENT --
 const inputHandler = () => {
   // determine maximum number of characters
-  const maxNrChar = 150;
+  const maxNrChar = MAX_CHARS;
 
   // determine number of characters currently typed
   const nrCharsTyped = textareaEl.value.length;
@@ -21,6 +25,16 @@ const inputHandler = () => {
 textareaEl.addEventListener("input", inputHandler);
 
 // -- FORM (SUBMIT) COMPONENT --
+
+const showVisualIndicator = (textCheck) => {
+  const className = textCheck === "valid" ? "form--valid" : "form--invalid";
+  // show valid indicator
+  formEl.classList.add(className);
+
+  // remove visual indicator
+  setTimeout(() => formEl.classList.remove(className), 2000);
+};
+
 const submitHandler = (event) => {
   // prevent default browser action (submitting form data to 'action'-address and refreshing page)
   event.preventDefault();
@@ -30,12 +44,58 @@ const submitHandler = (event) => {
 
   // validate text (e.g. check if #hashtag is present and text is long enough)
   if (text.includes("#") && text.length > 4) {
-    formEl.classList.add("form--valid");
-    setTimeout(() => formEl.classList.remove("form--valid"), 2000);
+    showVisualIndicator("valid");
   } else {
-    formEl.classList.add("form--invalid");
-    setTimeout(() => formEl.classList.remove("form--invalid"), 2000);
+    showVisualIndicator("invalid");
+
+    // foxus text area again - we can continue typing
+    textareaEl.focus();
+
+    // stop this function execution
+    return;
   }
+
+  // we have text, now extract other info from text
+
+  const hashtag = text.split(" ").find((word) => word.includes("#"));
+  const company = hashtag.substring(1);
+  const badgeLetter = company.substring(0, 1).toUpperCase();
+  const upvoteCount = 0;
+  const daysAgo = 0;
+
+  // new feedback item HTML
+  const feedbackItemHTML = `
+  <li class="feedback">
+            <button class="upvote">
+              <i class="fa-solid fa-caret-up upvote__icon"></i>
+              <span class="upvote__count">${upvoteCount}</span>
+            </button>
+            <section class="feedback__badge">
+              <p class="feedback__letter">${badgeLetter}</p>
+            </section>
+            <div class="feedback__content">
+              <p class="feedback__company">${company}</p>
+              <p class="feedback__text">
+                ${text}
+              </p>
+            </div>
+            <p class="feedback__date">${
+              daysAgo === 0 ? "NEW" : `${daysAgo}d`
+            }</p>
+          </li>
+  `;
+
+  // insert new feedback item in list
+  feedbackListEl.insertAdjacentHTML("beforeend", feedbackItemHTML);
+
+  // clear textarea
+  textareaEl.value = "";
+
+  // blur submit button - not focus
+  submitBtnEl.blur();
+
+  // reset counter
+  counterEl.textContent = MAX_CHARS;
 };
 
 formEl.addEventListener("submit", submitHandler);
