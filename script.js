3783940749402f6a686a1a4206e1473115fbea32
Danijel Klobucar <dklobucar@cadcam-group.eu>
2024-09-01 14:37:21 +0200

    Shield

--- script.js
+++ script.js
@@ -37,188 +37,198 @@ const renderFeedbackItem = (feedbackItem) => {
 };
 
 // -- COUNTER COMPONENT --
-const inputHandler = () => {
-  // determine maximum number of characters
-  const maxNrChar = MAX_CHARS;
 
-  // determine number of characters currently typed
-  const nrCharsTyped = textareaEl.value.length;
+(() => {
+  const inputHandler = () => {
+    // determine maximum number of characters
+    const maxNrChar = MAX_CHARS;
 
-  // calculate number of characters left
-  const charsLeft = maxNrChar - nrCharsTyped;
+    // determine number of characters currently typed
+    const nrCharsTyped = textareaEl.value.length;
 
-  //show number of characters left
-  counterEl.textContent = charsLeft;
-};
+    // calculate number of characters left
+    const charsLeft = maxNrChar - nrCharsTyped;
 
-textareaEl.addEventListener("input", inputHandler);
+    //show number of characters left
+    counterEl.textContent = charsLeft;
+  };
+
+  textareaEl.addEventListener("input", inputHandler);
+})();
 
 // -- FORM (SUBMIT) COMPONENT --
+(() => {
+  const showVisualIndicator = (textCheck) => {
+    const className = textCheck === "valid" ? "form--valid" : "form--invalid";
+    // show valid indicator
+    formEl.classList.add(className);
 
-const showVisualIndicator = (textCheck) => {
-  const className = textCheck === "valid" ? "form--valid" : "form--invalid";
-  // show valid indicator
-  formEl.classList.add(className);
-
-  // remove visual indicator
-  setTimeout(() => formEl.classList.remove(className), 2000);
-};
-
-const submitHandler = (event) => {
-  // prevent default browser action (submitting form data to 'action'-address and refreshing page)
-  event.preventDefault();
-
-  // get text from textarea
-  const text = textareaEl.value;
-
-  // validate text (e.g. check if #hashtag is present and text is long enough)
-  if (text.includes("#") && text.length > 4) {
-    showVisualIndicator("valid");
-  } else {
-    showVisualIndicator("invalid");
-
-    // foxus text area again - we can continue typing
-    textareaEl.focus();
-
-    // stop this function execution
-    return;
-  }
-
-  // we have text, now extract other info from text
-  const hashtag = text.split(" ").find((word) => word.includes("#"));
-  const company = hashtag.substring(1);
-  const badgeLetter = company.substring(0, 1).toUpperCase();
-  const upvoteCount = 0;
-  const daysAgo = 0;
-
-  // create feedback item object, render feedback item in list
-  const feedbackItem = {
-    hashtag: hashtag,
-    company: company,
-    badgeLetter: badgeLetter,
-    upvoteCount: upvoteCount,
-    daysAgo: daysAgo,
-    text: text,
+    // remove visual indicator
+    setTimeout(() => formEl.classList.remove(className), 2000);
   };
-  // render feedback item
-  renderFeedbackItem(feedbackItem);
 
-  // send feedback item to server
-  fetch(`${BASE_API_URL}/feedbacks`, {
-    method: "POST",
-    body: JSON.stringify(feedbackItem), //pretvarmo js file u JSON format
-    headers: {
-      Accept: "application/json", // what kind of data we can accept back - we accept json
-      "Content-Type": "application/json", // for json
-    },
-  }) // what we wont to do when we recieve response
+  const submitHandler = (event) => {
+    // prevent default browser action (submitting form data to 'action'-address and refreshing page)
+    event.preventDefault();
+
+    // get text from textarea
+    const text = textareaEl.value;
+
+    // validate text (e.g. check if #hashtag is present and text is long enough)
+    if (text.includes("#") && text.length > 4) {
+      showVisualIndicator("valid");
+    } else {
+      showVisualIndicator("invalid");
+
+      // foxus text area again - we can continue typing
+      textareaEl.focus();
+
+      // stop this function execution
+      return;
+    }
+
+    // we have text, now extract other info from text
+    const hashtag = text.split(" ").find((word) => word.includes("#"));
+    const company = hashtag.substring(1);
+    const badgeLetter = company.substring(0, 1).toUpperCase();
+    const upvoteCount = 0;
+    const daysAgo = 0;
+
+    // create feedback item object, render feedback item in list
+    const feedbackItem = {
+      hashtag: hashtag,
+      company: company,
+      badgeLetter: badgeLetter,
+      upvoteCount: upvoteCount,
+      daysAgo: daysAgo,
+      text: text,
+    };
+    // render feedback item
+    renderFeedbackItem(feedbackItem);
+
+    // send feedback item to server
+    fetch(`${BASE_API_URL}/feedbacks`, {
+      method: "POST",
+      body: JSON.stringify(feedbackItem), //pretvarmo js file u JSON format
+      headers: {
+        Accept: "application/json", // what kind of data we can accept back - we accept json
+        "Content-Type": "application/json", // for json
+      },
+    }) // what we wont to do when we recieve response
+      .then((response) => {
+        if (!response.ok) {
+          console.log("Something went wrong.");
+          return;
+        }
+
+        console.log("Successfully submitted.");
+      })
+      .catch((error) => console.log(error));
+
+    // insert new feedback item in list
+
+    // clear textarea
+    textareaEl.value = "";
+
+    // blur submit button - not focus
+    submitBtnEl.blur();
+
+    // reset counter
+    counterEl.textContent = MAX_CHARS;
+  };
+
+  formEl.addEventListener("submit", submitHandler);
+})();
+
+// -- FEEDBACK LIST COMPONENT --
+
+(() => {
+  const clickHandler = (event) => {
+    // get clicked HTML-element
+    const clickedEl = event.target;
+
+    // determine if user intended to upvote or expand
+    const upvoteIntention = clickedEl.className.includes("upvote");
+
+    // run the appropriate logic
+    if (upvoteIntention) {
+      // get the closest upvote button
+      const upvoteBtnEl = clickedEl.closest(".upvote");
+
+      // disable upvote button (prevent double-click, spam)
+      upvoteBtnEl.disabled = true;
+
+      // select the upvote count element within the upvote button
+      const upvoteCountEl = upvoteBtnEl.querySelector(".upvote__count");
+
+      // get currently displayed upvote count as number (+), ovaj plus odmah konvertira u integer
+      let upvoteCount = +upvoteCountEl.textContent;
+
+      // set upvote count increment by 1
+      upvoteCountEl.textContent = ++upvoteCount;
+    } else {
+      // expand the clicked feedback item
+      clickedEl.closest(".feedback").classList.toggle("feedback--expand");
+    }
+  };
+
+  feedbackListEl.addEventListener("click", clickHandler);
+
+  fetch(`${BASE_API_URL}/feedbacks`)
     .then((response) => {
       if (!response.ok) {
-        console.log("Something went wrong.");
-        return;
+        console.log("Nije uspješno.");
       }
-
-      console.log("Successfully submitted.");
+      return response.json();
     })
-    .catch((error) => console.log(error));
+    .then((data) => {
+      // remove spinner class
+      spinnerEl.remove();
 
-  // insert new feedback item in list
-
-  // clear textarea
-  textareaEl.value = "";
-
-  // blur submit button - not focus
-  submitBtnEl.blur();
-
-  // reset counter
-  counterEl.textContent = MAX_CHARS;
-};
-
-formEl.addEventListener("submit", submitHandler);
-
-// -- FEEDBACK LIST COMPONENT
-
-const clickHandler = (event) => {
-  // get clicked HTML-element
-  const clickedEl = event.target;
-
-  // determine if user intended to upvote or expand
-  const upvoteIntention = clickedEl.className.includes("upvote");
-
-  // run the appropriate logic
-  if (upvoteIntention) {
-    // get the closest upvote button
-    const upvoteBtnEl = clickedEl.closest(".upvote");
-
-    // disable upvote button (prevent double-click, spam)
-    upvoteBtnEl.disabled = true;
-
-    // select the upvote count element within the upvote button
-    const upvoteCountEl = upvoteBtnEl.querySelector(".upvote__count");
-
-    // get currently displayed upvote count as number (+), ovaj plus odmah konvertira u integer
-    let upvoteCount = +upvoteCountEl.textContent;
-
-    // set upvote count increment by 1
-    upvoteCountEl.textContent = ++upvoteCount;
-  } else {
-    // expand the clicked feedback item
-    clickedEl.closest(".feedback").classList.toggle("feedback--expand");
-  }
-};
-
-feedbackListEl.addEventListener("click", clickHandler);
-
-fetch(`${BASE_API_URL}/feedbacks`)
-  .then((response) => {
-    if (!response.ok) {
-      console.log("Nije uspješno.");
-    }
-    return response.json();
-  })
-  .then((data) => {
-    // remove spinner class
-    spinnerEl.remove();
-
-    // iterate over each element in feedbacks array and render it in list
-    data.feedbacks.forEach((feedbackItem) => renderFeedbackItem(feedbackItem));
-  })
-  .catch((error) => {
-    feedbackListEl.textContent = `Failed to fetch feedback items. Error message: ${error.message}`;
-  });
+      // iterate over each element in feedbacks array and render it in list
+      data.feedbacks.forEach((feedbackItem) =>
+        renderFeedbackItem(feedbackItem)
+      );
+    })
+    .catch((error) => {
+      feedbackListEl.textContent = `Failed to fetch feedback items. Error message: ${error.message}`;
+    });
+})();
 
 // -- HASHTAG LIST COMPONENT --
 
-const clickHandler2 = (event) => {
-  // get clicked HTML element
-  const clickedEl = event.target;
+(() => {
+  const clickHandler2 = (event) => {
+    // get clicked HTML element
+    const clickedEl = event.target;
 
-  // stop function if click happend in list, but outside buttons
-  if (clickedEl.className === "hashtags") return;
-
-  // extract company name
-  const companyNameFromHashtag = clickedEl.textContent
-    .substring(1)
-    .toLowerCase()
-    .trim();
-
-  // iterate over each feedback item in the list - feedbackListEl je HTML element, a ne array
-  feedbackListEl.childNodes.forEach((childNode) => {
-    // stop this iteration if it's a text node, to je 3
-    if (childNode.nodeType === 3) return;
+    // stop function if click happend in list, but outside buttons
+    if (clickedEl.className === "hashtags") return;
 
     // extract company name
-    const companyNameFromFeedbackItem = childNode
-      .querySelector(".feedback__company")
-      .textContent.toLowerCase()
+    const companyNameFromHashtag = clickedEl.textContent
+      .substring(1)
+      .toLowerCase()
       .trim();
 
-    // we want to show same companyNameFromHashtag and companyNameFromFeedbackItem
-    // remove feedback item from the list if company names are not equal
-    if (companyNameFromHashtag !== companyNameFromFeedbackItem) {
-      childNode.remove();
-    }
-  });
-};
+    // iterate over each feedback item in the list - feedbackListEl je HTML element, a ne array
+    feedbackListEl.childNodes.forEach((childNode) => {
+      // stop this iteration if it's a text node, to je 3
+      if (childNode.nodeType === 3) return;
 
-hashtagListEL.addEventListener("click", clickHandler2);
+      // extract company name
+      const companyNameFromFeedbackItem = childNode
+        .querySelector(".feedback__company")
+        .textContent.toLowerCase()
+        .trim();
+
+      // we want to show same companyNameFromHashtag and companyNameFromFeedbackItem
+      // remove feedback item from the list if company names are not equal
+      if (companyNameFromHashtag !== companyNameFromFeedbackItem) {
+        childNode.remove();
+      }
+    });
+  };
+
+  hashtagListEL.addEventListener("click", clickHandler2);
+})();
